adb46c25126565350c09b56f5a1ad5087f0c3893
Monica Salamanca <moni.salamanca@gmail.com>
2020-04-14 20:19:12 -0400

    created hook to separate som elogic and created Resultslist component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,27 +1,18 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMsg, setErrorMsg] = useState('')
+  const [searchApi, results, errorMsg] = useResults();
 
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'montreal'
-        }
-      })
-      setResults(response.data.businesses);
-    } catch(err) {
-      setErrorMsg('Something went wrong!');
-    }
-
+  const filterResultsByPrice = (price) => {
+    // price === '$' || "$$" || "$$$"
+    return results.filter((result) => {
+      return result.price === price
+    })
   }
 
   return (
@@ -33,6 +24,9 @@ const SearchScreen = () => {
       />
       {errorMsg ? <Text>{errorMsg}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   )
 }
